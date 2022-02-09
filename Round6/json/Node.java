3448853f0cb402fdbb9fb7985e30a877e7ccf197
Olli Lähde <47543970+runefullit@users.noreply.github.com>
2022-02-09 07:08:35 +0200

    Implemented printJson.

--- Round6/json/Node.java
+++ Round6/json/Node.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 public abstract class Node {
   public boolean isValue() {
     return this instanceof ValueNode;
@@ -18,8 +21,84 @@ public abstract class Node {
   }
 
   public void printJson() {
-    throw new UnsupportedOperationException("printJson has not been implemented!");
+      if (false){
+          throw new UnsupportedOperationException("printJson has not been implemented!");
+      } else {
+          System.out.print(printJson(this,""));
+      }
   }
+  
+/**
+ * Recursively build printable JSON representation starting from the root node it is called with. The function treats
+ * each level of recursion as a list that is joined together with appropriate padding.
+ * @param node
+ * @param indentation
+ * @param sb
+ * @return 
+ */  
+ private String printJson(Node node, String indentation){
+    ArrayList<String> strs = new ArrayList<>();
+    StringBuilder sb = new StringBuilder();
+    if(node.isObject()) {
+        ObjectNode objNode = (ObjectNode) node;
+        if (objNode.size() > 0){
+            // Indentation of the first line is taken care of by the segment calling it.
+            sb.append("{").append(NL);
+            for(String name : objNode) {
+                strs.add(String.format("\"%s\": %s", name, printJson(objNode.get(name), indentation + "  ")));
+            }
+            // Taking care of the indentation of the first line in called segment. Indentation needed is
+            // one level deeper than passed by indentation variable. 
+            sb.append(indentation).append("  ").append(String.join(","+NL+indentation+"  ", strs));
+            sb.append(NL).append(indentation).append("}");
+    } else {
+            // Empty objects are exit points of the recursion.
+            sb.append("{}");
+      }
+    }
+    else if(node.isArray()) {
+        ArrayNode arrNode = (ArrayNode) node;
+        if (arrNode.size() > 0){
+            // Indentation of the first line is taken care of by the section calling it.
+            sb.append("[").append(NL);
+            for(Node aNode : arrNode) {
+                strs.add(printJson(aNode, indentation + "  "));
+            }
+            // Indentation needed here is the saved indentation level + one more level. This was
+            // simply easier to build than removing padding from the indentation string for other parts.
+            sb.append(indentation).append("  ").append(String.join(","+NL+indentation+"  ", strs));
+            sb.append(NL).append(indentation).append("]");
+        } else {
+            // Empty arrays are exit points of recursion.
+            sb.append("[]");
+        }
+    }
+    else if(node.isValue()) {
+      ValueNode valNode = (ValueNode) node;
+      // Value nodes are exit points of the recursion.
+      sb.append(getValueNodeString(valNode));
+    }
+    return sb.toString();
+ }
+ 
+ /**
+  * Find the valueNode's stored value and return it as a string.
+  * @param node
+  * @return 
+  */
+ private String getValueNodeString(ValueNode node){
+    String valStr = "null";
+    if(node.isNumber()) {
+    valStr = numberToString(node.getNumber());
+    }
+    else if(node.isBoolean()) {
+    valStr = Boolean.toString(node.getBoolean());
+    }
+    else if(node.isString()) {
+    valStr = "\"" + node.getString() + "\"";
+    }
+    return valStr;
+ }
 
   private static final String NL = System.lineSeparator();
 
