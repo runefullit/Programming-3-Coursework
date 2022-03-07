a5a2ed259bdd5172bd9ca40d7606556c5d749fbe
Olli Lähde <olli.e.lahde@gmail.com>
2022-03-07 16:59:00 +0200

    Added conversion from F to C, added query to choose only one with.

--- Round10/GIT_EX/temperature.py
+++ Round10/GIT_EX/temperature.py
@@ -2,13 +2,18 @@
 def main():
 
     try:
+        tdir = input("Select transform direction (CF) or (FC): ")
         input_temp = float(input("Input temperature: "))
     except:
         print("Not valid input")
         exit()
 
-
-    print("{} C = {} F".format(input_temp, t_transform(input_temp, 'c')))
+    if tdir == "CF":
+        print("{} C = {} F".format(input_temp, t_transform(input_temp, 'c')))
+    elif tdir == "FC":
+       print("{} F = {} C".format(input_temp, t_transform(input_temp, 'f')))
+    else:
+        print("Not valid direction.")
 
 
 
@@ -17,6 +22,8 @@ def t_transform(temp, type):
 
     if type == 'c':
         return float(temp * 9/5 + 32)
+    elif type == 'f':
+        return float((temp-32) * 5/9)
 
     return None
 
