1fa2df5327b68af2d3353f16420b882e6461f9fa
Olli Lähde <olli.e.lahde@gmail.com>
2022-03-21 16:07:04 +0200

    Removed mocking from unit tests.

--- Round11/junitorder/src/test/java/fi/tuni/prog3/junitorder/EntryTest.java
+++ Round11/junitorder/src/test/java/fi/tuni/prog3/junitorder/EntryTest.java
@@ -1,45 +1,44 @@
 package fi.tuni.prog3.junitorder;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
+
 public class EntryTest {
-
-    @Mock
-    Order.Item item;
-
+    private Order.Item item;
 
     @Test
     public void entryItemMatchesGivenParameter() {
+        String name = "name";
+        double price = Math.sqrt(2);
+        this.item = new Order.Item(name, price);
+
         Order.Entry entry = new Order.Entry(item, 1);
+        
         assertEquals(item, entry.getItem());
     }
 
     @Test
     public void entryItemNameIsExpectedItemName() {
         String name = "name";
-        when(item.getName()).thenReturn(name);
+        double price = Math.sqrt(2);
+        this.item = new Order.Item(name, price);
 
-        Order.Entry entry = new Order.Entry(item, 1);
+        Order.Entry entry = new Order.Entry(this.item, 1);
 
         assertEquals(name, entry.getItemName());
     }
 
     @Test
     public void entryItemPriceIsExpectedItemPrice() {
+        String name = "name";
         double price = Math.sqrt(2);
+        this.item = new Order.Item(name, price);
 
-        when(item.getPrice()).thenReturn(price);
-
-        Order.Entry entry = new Order.Entry(item, 1);
+        Order.Entry entry = new Order.Entry(this.item, 1);
 
         assertEquals(price, entry.getUnitPrice());
     }
@@ -47,14 +46,23 @@ public class EntryTest {
     @ParameterizedTest
     @ValueSource(ints = {1,5,100,11231})
     public void countMatchesGivenLegalParameter(int count) {
-        Order.Entry entry = new Order.Entry(item, count);
+        String name = "name";
+        double price = Math.sqrt(2);
+        this.item = new Order.Item(name, price);
+
+        Order.Entry entry = new Order.Entry(this.item, count);
+
         assertEquals(count, entry.getCount());
     }
 
     @ParameterizedTest
     @ValueSource(ints = {-1, 0})
     public void countThrowsIllegalArgumentExceptionWithNonPositiveCount(int count) {
-        assertThrows(IllegalArgumentException.class, () -> new Order.Entry(item, count));
+        String name = "name";
+        double price = Math.sqrt(2);
+        this.item = new Order.Item(name, price);
+
+        assertThrows(IllegalArgumentException.class, () -> new Order.Entry(this.item, count));
     }
 
     @Test
@@ -62,12 +70,11 @@ public class EntryTest {
         String name = "name";
         double price = Math.sqrt(2);
         int count = 17;
-
-        when(item.toString()).thenReturn(String.format("Item(%s, %.2f)", name, price));
+        this.item = new Order.Item(name, price);
 
         Order.Entry entry = new Order.Entry(new Order.Item(name, price), count);
 
-        assertEquals(String.format("%d units of %s", count, item.toString()), entry.toString());
+        assertEquals(String.format("%d units of %s", count, this.item.toString()), entry.toString());
     }
 
 }
