00aa527ebe429787698bbadb320c45a745d3aa09
Olli Lähde <olli.e.lahde@gmail.com>
2022-03-27 02:11:23 +0200

    Removed unused imports.

--- Round12/wordle/src/main/java/fi/tuni/prog3/wordle/ViewBuilder.java
+++ Round12/wordle/src/main/java/fi/tuni/prog3/wordle/ViewBuilder.java
@@ -2,7 +2,6 @@ package fi.tuni.prog3.wordle;
 
 import javafx.scene.Parent;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 
 public class ViewBuilder extends Parent {
--- Round12/wordle/src/main/java/fi/tuni/prog3/wordle/Wordle.java
+++ Round12/wordle/src/main/java/fi/tuni/prog3/wordle/Wordle.java
@@ -4,8 +4,6 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 
 public class Wordle extends Application {
     @Override
