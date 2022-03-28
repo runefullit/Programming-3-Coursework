b9281ab5a2e3d02a9a0e7d6fd161905a2ecd877f
Olli Lähde <olli.e.lahde@gmail.com>
2022-03-28 18:03:42 +0300

    Added a record for maintaining info about the game.

--- Round12/wordle/src/main/java/fi/tuni/prog3/wordle/WordleModel.java
+++ Round12/wordle/src/main/java/fi/tuni/prog3/wordle/WordleModel.java
@@ -1,9 +1,44 @@
 package fi.tuni.prog3.wordle;
 
-public record WordleModel(
-        int currentColumn,
-        String word,
-        String alphabet,
-        boolean wordGuessed,
-        boolean gameOver
-) {}
+
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.*;
+import javafx.beans.value.ObservableBooleanValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public record WordleModel() {
+        static int wordLength = 6;
+        static List<List<LetterModel>> letters = buildLetters();
+        static ObservableList<Character> word = FXCollections.observableArrayList();
+        static Map<Character, ObjectProperty<LetterStatus>> alphabet = IntStream.rangeClosed('A', 'Z')
+                .collect(HashMap::new,
+                        (map, c) -> map.put((char)c, new SimpleObjectProperty<>(LetterStatus.UNLOCKED)),
+                        HashMap::putAll);
+        static IntegerProperty currentRow = new SimpleIntegerProperty(0);
+        static BooleanProperty darkMode = new SimpleBooleanProperty(false);
+        static BooleanProperty wordGuessed = new SimpleBooleanProperty(false);
+        static ObservableBooleanValue gameOver = Bindings.createBooleanBinding(
+                () -> (currentRow.getValue() > 5 || wordGuessed.getValue()),
+                currentRow,
+                wordGuessed);
+
+
+        private static List<List<LetterModel>> buildLetters() {
+                int guesses = 6;
+                List<List<LetterModel>> letters = new ArrayList<>(guesses);
+                for(int i = 0; i < guesses; i++) {
+                        letters.add(
+                                IntStream
+                                        .range(0, wordLength)
+                                        .mapToObj(LetterModel::new)
+                                        .collect(Collectors.toList())
+                                );
+                }
+                return letters;
+        }
+}
