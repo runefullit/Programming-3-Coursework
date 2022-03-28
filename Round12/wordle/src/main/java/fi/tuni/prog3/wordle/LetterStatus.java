af5e7b80d25bd21d6c51c0c96bdaf34e9ea286ec
Olli Lähde <olli.e.lahde@gmail.com>
2022-03-28 16:06:59 +0300

    Added enum for tracking and managing CSS selectinos of Letter Boxes.

---
+++ Round12/wordle/src/main/java/fi/tuni/prog3/wordle/LetterStatus.java
@@ -0,0 +1,40 @@
+package fi.tuni.prog3.wordle;
+
+import javafx.beans.property.ObjectProperty;
+import javafx.css.PseudoClass;
+import javafx.scene.Node;
+
+public enum LetterStatus {
+    EMPTY, UNLOCKED, WRONG, PRESENT, CORRECT;
+
+    private final PseudoClass emptyPseudoclass = PseudoClass.getPseudoClass("empty");
+    private final PseudoClass unlockedPseudoClass = PseudoClass.getPseudoClass("unlocked");
+    private final PseudoClass wrongPseudoClass = PseudoClass.getPseudoClass("wrong");
+    private final PseudoClass presentPseudoClass = PseudoClass.getPseudoClass("present");
+    private final PseudoClass correctPseudoClass = PseudoClass.getPseudoClass("correct");
+
+    public void updatePseudoClass(Node node, LetterStatus letterStatus) {
+        node.pseudoClassStateChanged(emptyPseudoclass, false);
+        node.pseudoClassStateChanged(unlockedPseudoClass, false);
+        node.pseudoClassStateChanged(wrongPseudoClass, false);
+        node.pseudoClassStateChanged(presentPseudoClass, false);
+        node.pseudoClassStateChanged(correctPseudoClass, false);
+
+        switch (letterStatus) {
+            case EMPTY:
+                node.pseudoClassStateChanged(emptyPseudoclass, true);
+            case UNLOCKED:
+                node.pseudoClassStateChanged(unlockedPseudoClass, true);
+            case WRONG:
+                node.pseudoClassStateChanged(wrongPseudoClass, true);
+            case PRESENT:
+                node.pseudoClassStateChanged(presentPseudoClass, true);
+            case CORRECT:
+                node.pseudoClassStateChanged(correctPseudoClass, true);
+        }
+    }
+
+    public void addPseudoClass(Node node, ObjectProperty<LetterStatus> property) {
+        property.addListener( a -> updatePseudoClass(node, property.get()));
+    }
+}
